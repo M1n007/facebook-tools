b079c16df582eba734f39d938d8fbbe641633872
Amin <amin4udin@gmail.com>
2019-03-10 18:16:05 +0700

    update lib.

--- index.js
+++ index.js
@@ -1,82 +1,29 @@
 const fetch = require("node-fetch");
-const moment = require("moment");
+const friendList = require("./lib/getFriendList");
+const lastActive = require("./lib/lasActive");
+const unfriend = require("./lib/unfriend");
 
-const token = "";
+const token =
+  "EAAAAAYsX7TsBANGnESOdezLjLGSWhHndaS2HBqXitvrHzZB0MF87wmc1ssXYM1IORC1KaUjvrHExlCLQkiOn4ZAFvuZCVU4gN27TLNZBQSRHb19aROyckRDqPN8Ob1PFKnL3b3RetgFB27Lwe3oM3cpIJea1iBM3qJIerbNZCz0uY1uVeWITKYIaHCO6HMpW0ZAuZCDkM9OlxyItl0dJR2ZA";
 
-function friendList() {
-  return new Promise((resolve, reject) => {
-    fetch("https://graph.facebook.com/me/friends?access_token=" + token)
-      .then(res => res.json())
-      .then(response => {
-        resolve(response.data);
-      })
-      .catch(err => {
-        reject(err);
-      });
-  });
-}
-
-function lastActive(data) {
-  return new Promise((resolve, reject) => {
-    fetch(
-      "https://graph.facebook.com/" +
-        data +
-        "/feed?access_token=" +
-        token +
-        "&limit=1"
-    )
-      .then(res => res.json())
-      .then(lastactive => {
-        resolve(lastactive);
-      })
-      .catch(err => {
-        reject(err);
-      });
-  });
-}
-
-async function unfriend() {
-  return new Promise((resolve, reject) => {
-    fetch(
-      "https://graph.facebook.com/me/friends?uid=" +
-        id +
-        "&access_token=" +
-        token,
-      { method: "DELETE" }
-    )
-      .then(res => res.json())
-      .then(respon => {
-        resolve(respon);
-      })
-      .catch(err => {
-        reject(err);
-      });
-  });
-}
-
-(async () => {
-  const getFriendList = await friendList();
-  getFriendList
-    .forEach(data => {
+friendList(token)
+  .then(res => {
+    res.forEach(data => {
       const id = data.id;
       const name = data.name;
-      lastActive(id)
-        .then(res => {
-          const date = res.data[0].created_time;
-
-          const newDate = moment(date).format("Y");
-          console.log(name);
-          if (newDate < 2017) {
-            console.log("User" + " " + name + " " + [INACTIVE]);
-          } else {
-            console.log("User" + " " + name + " " + [ACTIVE]);
-          }
-        })
-        .catch(err => {
-          console.log("failed. try again");
-        });
-    })
-    .catch(err => {
-      console.log("failed. request limit");
+      lastActive(id, token).then(log => {
+        if (log < 2018) {
+          unfriend(id, token).then(deleted => {
+            console.log(
+              "USER" + " " + name + " " + "[INACTIVE]" + "" + "UNFRIEND SUCCESS"
+            );
+          });
+        } else {
+          console.log("USER" + " " + name + " " + "[ACTIVE]");
+        }
+      });
     });
-})();
+  })
+  .catch(err => {
+    console.log(err);
+  });
